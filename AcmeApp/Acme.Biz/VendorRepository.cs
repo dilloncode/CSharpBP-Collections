526540fc61132eaae27f2f9a4c52e44de7c28ab2
mdillon <mikedillon@gmx.com>
2018-07-02 15:56:05 -0400

    Generic lists

--- AcmeApp/Acme.Biz/VendorRepository.cs
+++ AcmeApp/Acme.Biz/VendorRepository.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Acme.Biz
 {
     public class VendorRepository
     {
+        private List<Vendor> vendors;
         /// <summary>
         /// Retrieve one vendor.
         /// </summary>
@@ -27,6 +25,20 @@ namespace Acme.Biz
                 vendor.Email = "abc@abc.com";
             }
             return vendor;
+        }
+
+        public List<Vendor> Retrieve()
+        {
+            if (vendors == null)
+            {
+                vendors = new List<Vendor>();
+
+                vendors.Add(new Vendor() { VendorId = 1, CompanyName = "ABC Company", Email = "abc@abc.com" });
+                vendors.Add(new Vendor() { VendorId = 2, CompanyName = "XYZ Company", Email = "xyz@xyz.com" });
+            }
+            Console.WriteLine(vendors);
+
+            return vendors;
         }
 
         public T RetrieveValue<T>(string sql, T defaultValue)
